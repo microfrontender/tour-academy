e1ac066cbda4a719a3a5214abc7409a20596c791
Kalinovsky <kalinovsky.coop@gmail.com>
2019-05-04 00:44:44 +0700

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,28 @@
 
 import "jquery";
 import 'reset-css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel';
 import '../sass/index.sass';
+
+var owl = $(".review__carousel");
+owl.owlCarousel({
+    loop: true,
+    merge: true,
+    nav: false,
+    dots: false,
+    autoWidth: true,
+    center: true,
+    items: 1,
+    touchDrag: false,
+    mouseDrag: false
+});
+
+
+$('.review__nav_prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+});
+$('.review__nav_next').click(function() {
+    owl.trigger('next.owl.carousel');
+});
