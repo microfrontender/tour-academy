82a3b36b9a329844dc7a90696c91bc53bfb4596b
Kalinovsky <kalinovsky.coop@gmail.com>
2019-05-11 02:06:18 +0700

    final

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ import WOW from 'wowjs';
 import 'animate.css';
 import '../sass/index.sass';
 import Inputmask from "inputmask";
+<<<<<<< HEAD
 
 
 // wow init
@@ -24,6 +25,8 @@ var wow = new WOW.WOW(
     }
   );
 wow.init();
+=======
+>>>>>>> c05adb9... Full
 
 // BEGIN NAVBAR
 
@@ -177,6 +180,7 @@ $("#feedback-form, #popup-call, #popup-form").submit(function() {
 
 // BEGIN END MAIL
 
+<<<<<<< HEAD
 // phone mask
 var im = new Inputmask("+7 (999) 999-99-99");
 im.mask(document.querySelectorAll("#popup-phone"))
@@ -189,3 +193,7 @@ $(".nav__link").on("click", function (event) {
         top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 500);
 });
+=======
+var im = new Inputmask("+7 (999) 999-99-99");
+im.mask(document.querySelectorAll("#popup-phone"))
+>>>>>>> c05adb9... Full
